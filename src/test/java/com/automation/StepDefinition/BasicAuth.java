20d24cd48a66d672ca96312ebf75aca5e55d3806
BaneleMlamleli <mlamlelibanele@yahoo.com>
2023-06-13 00:11:38 +0200

    Added the prompt alert to manage the JS Alert pop-up so that I can be able to enter password and username. I still need to fix this, but the momemnt this is the only solution I have

--- src/test/java/com/automation/StepDefinition/BasicAuth.java
+++ src/test/java/com/automation/StepDefinition/BasicAuth.java
@@ -1,15 +1,16 @@
 package com.automation.StepDefinition;
 
-import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.*;
 
 public class BasicAuth {
     WebDriver webDriver = null;
     public String url;
+    Alert alert = null;
 
     @Given("a user is on the website home page")
     public void a_user_is_on_the_website_home_page() {
@@ -22,30 +23,31 @@ public class BasicAuth {
     @When("login pop-up message box is displayed")
     public void login_pop_up_message_box_is_displayed() {
         webDriver.findElement(By.xpath("//a[normalize-space()='Basic Auth']")).click();
-    }
-
-    @And("^user enters the (.*) and (.*)$")
-    public void user_enters_the_password_and_username(String password, String username) {
-        System.out.println("enter password and username");
-        url = "https://"+username+":"+password+"@the-internet.kineticskunk.co.za/basic_auth";
-    }
-    
-    @Then("user clicks on the sign in button")
-    public void user_clicks_on_the_sign_in_button() {
-        System.out.println("webDriver.get(url)");
-        webDriver.get(url);
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
+    @And("^user enters the (.*) and (.*)$")
+    public void user_enters_the_password_and_username(String password, String username) {
+        alert = webDriver.switchTo().alert();
+        alert.sendKeys(password);
+        alert.sendKeys(username);
+        // url = "https://"+username+":"+password+"@the-internet.kineticskunk.co.za/basic_auth";
+    }
+    
+    @Then("user clicks on the sign in button")
+    public void user_clicks_on_the_sign_in_button() {
+        alert.accept();
+    }
     
     @And("login status")
     public void login_status() {
         System.out.println("The page title is " + webDriver.getTitle());        
         System.out.println("Website text: " + webDriver.findElement(By.tagName("p")).getText());
-        // if (webDriver.findElement(By.xpath("//h3[normalize-space()='Basic Auth']")).isDisplayed()) {
+        // if(webDriver.findElement(By.xpath("//h3[normalize-space()='Basic Auth']")).isDisplayed()) {
         //     System.out.println("Successfully logged in");
         // } else {
         //     System.out.println("Incorrect login credentials"); //Not authorized
