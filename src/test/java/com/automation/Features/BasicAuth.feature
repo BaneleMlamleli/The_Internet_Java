00e636d347dbbf13de67f02f8fb2430cd2afed33
Banele Mlamleli <banele@kineticskunk.com>
2023-06-14 15:27:07 +0200

    BasicAuth.feature and BasicAuth.java I could not automate using the JavaScript Prompt alert. The work around I implemented is entering the login credentials in the url, this skips the Alert but still authenticate the credentials. This works but it's not really the solution is it's not testing the alert pop-up prompt. I still need to figure this out

--- src/test/java/com/automation/Features/BasicAuth.feature
+++ src/test/java/com/automation/Features/BasicAuth.feature
@@ -5,11 +5,7 @@ Feature: Login windows for authentication
         Given a user is on the website home page
 
     Scenario Outline: Enter correct and incorrect login credentials for authentication
-        When login pop-up message box is displayed
-        And user enters the <password> and <username>
+        When user clicks on basic auth
+        And user enters the password and username
         Then user clicks on the sign in button
         And login status
-
-            | password   | username   |
-            | admin_user | adminXYZ01 |
-            | admin      | admin      |
--- src/test/java/com/automation/StepDefinition/BasicAuth.java
+++ src/test/java/com/automation/StepDefinition/BasicAuth.java
@@ -1,6 +1,4 @@
 package com.automation.StepDefinition;
-
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,44 +7,40 @@ import io.cucumber.java.en.*;
 
 public class BasicAuth {
     WebDriver webDriver = null;
-    public String url;
-    Alert alert = null;
-
+    public String url, completeUrl;
+    String password = "admin";
+    String username = "admin";
+    // Alert alert;
+    
     @Given("a user is on the website home page")
     public void a_user_is_on_the_website_home_page() {
         System.setProperty("webdriver.chrome.driver", "c:\\browser_webdrivers\\chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
-        webDriver.navigate().to("https://the-internet.kineticskunk.co.za/");
     }
 
-    @When("login pop-up message box is displayed")
-    public void login_pop_up_message_box_is_displayed() {
-        webDriver.findElement(By.xpath("//a[normalize-space()='Basic Auth']")).click();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    @When("user clicks on basic auth")
+    public void user_clicks_on_basic_auth() {
+        url = "@"+ "the-internet.kineticskunk.co.za/basic_auth";
     }
-
-    @And("^user enters the (.*) and (.*)$")
-    public void user_enters_the_password_and_username(String password, String username) {
-        alert = webDriver.switchTo().alert();
-        alert.sendKeys(password);
-        alert.sendKeys(username);
-        // url = "https://"+username+":"+password+"@the-internet.kineticskunk.co.za/basic_auth";
+    
+    @And("user enters the password and username")
+    public void user_enters_the_password_and_username() {
+        completeUrl = "https://" + username + ":" + password + url;
+        System.out.println("completeUrl: " + completeUrl);
     }
     
     @Then("user clicks on the sign in button")
     public void user_clicks_on_the_sign_in_button() {
-        alert.accept();
+        webDriver.get(completeUrl);
     }
     
     @And("login status")
     public void login_status() {
-        System.out.println("The page title is " + webDriver.getTitle());        
-        System.out.println("Website text: " + webDriver.findElement(By.tagName("p")).getText());
+        String title = webDriver.getTitle();
+        System.out.println("The page title is "+title);
+        String text = webDriver.findElement(By.tagName("p")).getText();
+        System.out.println("The test present in page is ==> "+text);
         // if(webDriver.findElement(By.xpath("//h3[normalize-space()='Basic Auth']")).isDisplayed()) {
         //     System.out.println("Successfully logged in");
         // } else {
