21e7e8a3a198b033027f6d4d915f5010533f8b62
Banele Mlamleli <banele@kineticskunk.com>
2023-06-23 15:44:51 +0200

    email check done and email validation done

--- src/test/java/com/automation/Runners/TestMainClass.java
+++ src/test/java/com/automation/Runners/TestMainClass.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
         monochrome = true, 
         plugin = { "pretty", "html:target/HtmlReports.html", "json:target/JsonReports.json",
                 "junit:target/XmlReport.xml" }, 
-        tags = "@FloatingMenu"
+        tags = "@ForgotPassword"
 )
 public class TestMainClass {
     
