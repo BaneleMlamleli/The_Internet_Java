bc46dd41c6468af250b3e92b1d657f27cba2e07e
Banele Mlamleli <banele@kineticskunk.com>
2023-06-28 11:17:01 +0200

    Infinite scroll tested and passed

--- src/test/java/com/automation/StepDefinition/Hover.java
+++ src/test/java/com/automation/StepDefinition/Hover.java
@@ -16,18 +16,18 @@ public class Hover {
     WebDriver webDriver = null;
     Actions actions = null;
     
-    @Given("a user is on the website home page")
-    public void a_user_is_on_the_website_home_page() {
-        System.setProperty("webdriver.chrome.driver", "c:\\browser_webdrivers\\chromedriver.exe");
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        webDriver.navigate().to("https://the-internet.kineticskunk.co.za/");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    // @Given("a user is on the website home page")
+    // public void a_user_is_on_the_website_home_page() {
+    //     System.setProperty("webdriver.chrome.driver", "c:\\browser_webdrivers\\chromedriver.exe");
+    //     webDriver = new ChromeDriver();
+    //     webDriver.manage().window().maximize();
+    //     webDriver.navigate().to("https://the-internet.kineticskunk.co.za/");
+    //     try {
+    //         Thread.sleep(2000);
+    //     } catch (InterruptedException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
     @When("user clicks on Hovers")
     public void user_clicks_on_hover() {
---
+++ src/test/java/com/automation/StepDefinition/InfiniteScroll.java
@@ -0,0 +1,47 @@
+package com.automation.StepDefinition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class InfiniteScroll {
+    WebDriver webDriver = null;
+    
+    @Given("a user is on the website home page")
+    public void a_user_is_on_the_website_home_page() {
+        System.setProperty("webdriver.chrome.driver", "c:\\browser_webdrivers\\chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.navigate().to("https://the-internet.kineticskunk.co.za/");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @When("user clicks on Infinite Scroll")
+    public void user_clicks_on_infinite_scroll() {
+        webDriver.findElement(By.xpath("//a[normalize-space()='Infinite Scroll']")).click();
+    }
+
+    @Then("confirm infinite scrolling")
+    public void confirm_infinite_scrolling() {
+        boolean confirmDisplay = webDriver.findElement(By.xpath("//h3[normalize-space()='Infinite Scroll']")).isDisplayed();
+        if (confirmDisplay) {
+            System.out.println("Infinite scrolling confirmed. Test passed");
+        }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        webDriver.close();
+        webDriver.quit();
+    }
+}
